560d2853cfe9d84b5a83685ee218f97d17dd8e0b
John Dailey <johnjdailey@gmail.com>
2020-05-08 02:53:06 -0400

    oop and and classes

---
+++ buyholdsell/polos.py
@@ -0,0 +1,22 @@
+
+
+class Polo:
+    def __init__(self, size, color, price=69.99):
+        self.size = size
+        self.color = color
+        self.price = price
+
+    def wash(self):
+        print(f"WASHING THE {self.size.upper()} {self.color.upper()} POLO!")
+
+
+if __name__ == '__main__':
+    # only execute if run from the command-line (not when imported)
+
+    polo = Polo(size='Large', color="Blue")
+    print(polo.size, polo.color)
+    polo.wash()
+
+    polo2 = Polo(size='Small', color="Yellow")
+    print(polo2.size, polo2.color)
+    polo2.wash()
