cf96ad8b85366aa347504afe2b4417571e8f218b
John Dailey <johnjdailey@gmail.com>
2020-05-17 08:11:39 -0400

    Dedicate this unittest to Mike

--- test/RSI_test.py
+++ test/RSI_test.py
@@ -3,22 +3,36 @@
 
 import unittest
 
-from buyholdsell.RSI import buyholdsell
+# from buyholdsell.RSI import buyholdsell
 
 # OBJECTIVE: test the buyholdsell() function from the buyholdsell/RSI.py file
+
+def buyholdsell(n):
+    """
+    Param n is a number.
+    Function will decide if the number is worth buying, holding, or selling.
+    """
+
+    if (n) in range(1, 30):
+        return "I'm buying"
+    elif (n) in range(30, 71):
+        return "I'm holding"
+    elif (n) in range(70, 101):
+        return "I'm selling"
+    else:
+        return "Your number wasn't in the correct range"
 
 class TestRSI(unittest.TestCase):
     
     def test_buyholdsell(self):
-        # expect that it prints the correct statement for the correct range
-        self.assertTrue(buyholdsell(1), 1)
+        """Test the output for condition"""
+        n1 = 5
+        n2 = 35
+        n3 = 75
+        self.assertEqual(buyholdsell(n1), "I'm buying")
+        self.assertEqual(buyholdsell(n2), "I'm holding")
+        self.assertEqual(buyholdsell(n3), "I'm selling")
         
-        # https://docs.python.org/3/library/exceptions.html#ValueError
-        with self.assertRaises(ValueError):
-            buyholdsell(101)
-        
-        with self.assertRaises(ValueError):
-            buyholdsell(0)
 
 if __name__ == "__main__":
     unittest.main()
